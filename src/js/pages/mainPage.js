b24eef5dc4908323932c553c04acea8df4509879
Zvorygin Egor <egoreq@bk.ru>
2020-11-06 15:55:36 +0400

    feat: add scene and sphere

--- src/js/pages/mainPage.js
+++ src/js/pages/mainPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-function MainPage() {
+import * as THREE from 'three';
+
+/*function MainPage() {    
     return (
         <section className="main-page">
             <div className="main-page__text">HELLO</div>
@@ -10,6 +11,50 @@ function MainPage() {
             </Link>
         </section>
     );
-}
+    return <div className="threejs-root">{renderer.domElement}</div>;
+}*/
 
+class MainPage extends React.Component {
+    mount = React.createRef();
+
+    componentDidMount() {
+        let scene = new THREE.Scene();
+
+        let camera = new THREE.PerspectiveCamera(
+            90,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        );
+
+        let renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        this.mount.current.appendChild(renderer.domElement);
+
+        let geometry = new THREE.SphereGeometry(1, 32, 32); //Сфара
+
+        let material = new THREE.MeshNormalMaterial(); //Навешиваем на нее материал
+
+        let sphere = new THREE.Mesh(geometry, material); //Объединяем
+
+        const light = new THREE.AmbientLight(0x404040); // soft white light
+        scene.add(light);
+
+        scene.add(sphere);
+        camera.position.z = 3;
+
+        /*let animate = function render() {
+            requestAnimationFrame(render);
+            sphere.rotation.x += 0.01;
+            sphere.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        };
+        animate();*/
+        renderer.render(scene, camera);
+    }
+
+    render() {
+        return <div ref={this.mount} />;
+    }
+}
 export default MainPage;
