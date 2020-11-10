e060e7debc9f0b66764f3bba03abda646268e9e5
Zvorygin Egor <egoreq@bk.ru>
2020-11-10 14:58:10 +0400

    feat: fix appjs

--- src/js/App.js
+++ src/js/App.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 
 import MainPage from './pages/mainPage';
 import SliderPage from './pages/sliderPage';
-//import NewPage from './pages/newMainPage';
 import store from './redux/store';
 
 function App() {
@@ -13,8 +12,6 @@ function App() {
             <Router>
                 <Route exact path="/" component={MainPage} />
                 <Route path="/slider" component={SliderPage} />
-
-                {/*<Route path="/newpage" component={NewPage} />*/}
             </Router>
         </Provider>
     );
