2336909b5d0830011c76164ffbbbe536996e3361
Zvorygin Egor <egoreq@bk.ru>
2020-11-11 09:45:09 +0400

    feat: add camera move and resize

--- src/js/pages/mainPage.js
+++ src/js/pages/mainPage.js
@@ -7,6 +7,18 @@ import Arrow from '../models/arrow';
 
 class MainPage extends React.Component {
     locations = [];
+    scene;
+    camera;
+    renderer;
+    mainSphere;
+    mouseDownMouseX;
+    mouseDownMouseY;
+    mouseDownLon;
+    mouseDownLat;
+    lat = 0;
+    lon = 0;
+    phi = 0;
+    theta = 0;
     mount = React.createRef();
     async componentDidMount() {
         this.scene = new THREE.Scene();
@@ -38,14 +50,15 @@ class MainPage extends React.Component {
         this.secondSphere.mesh.position.z = -2;
         this.scene.add(this.secondSphere.mesh);
 
-        this.planeGeometry = new THREE.PlaneGeometry(4, 20);
-        this.planeMaterial = new THREE.MeshBasicMaterial({
+        const planeGeometry = new THREE.PlaneGeometry(4, 20);
+        const planeMaterial = new THREE.MeshBasicMaterial({
             color: 0x00ff00,
         });
-        this.plane = new THREE.Mesh(this.planeGeometry, this.planeMaterial);
-        this.plane.rotation.x = (-90 * Math.PI) / 180;
-        this.plane.position.y = -1;
-        this.scene.add(this.plane);
+
+        const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.rotation.x = (-90 * Math.PI) / 180;
+        plane.position.y = -1;
+        this.scene.add(plane);
 
         /*this.camera.rotation.y = (45 * Math.PI) / 180;
         this.camera.rotation.x = (-45 * Math.PI) / 180;
@@ -54,16 +67,51 @@ class MainPage extends React.Component {
         this.camera.position.x = 3;
         this.camera.position.z = 2;*/
 
-        /*const animate = function render() {
-            requestAnimationFrame(render);
-            renderer.render(this.scene, this.camera);
-        };
-        animate();*/
+        this.camera.target = new THREE.Vector3(0, 0, 0);
+        this.mount.current.addEventListener('mousedown', this.onPointerStart);
+        this.mount.current.addEventListener('mousemove', this.onPointerMove);
+        this.mount.current.addEventListener('mouseup', this.onPointerUp);
+        window.addEventListener('resize', this.onWindowResize);
         this.animate();
     }
-
+    onWindowResize = () => {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    };
+    onPointerStart = (event) => {
+        let clientX = event.clientX || event.touches[0].clientX;
+        let clientY = event.clientY || event.touches[0].clientY;
+        this.mouseDownMouseX = clientX;
+        this.mouseDownMouseY = clientY;
+        this.mouseDownLon = this.lon;
+        this.mouseDownLat = this.lat;
+    };
+    onPointerMove = (event) => {
+        if (!this.mouseDownMouseX) return;
+        let clientX = event.clientX || event.touches[0].clientX;
+        let clientY = event.clientY || event.touches[0].clientY;
+        this.lon =
+            ((this.mouseDownMouseX - clientX) * this.camera.fov) / 600 +
+            this.mouseDownLon;
+        this.lat =
+            ((clientY - this.mouseDownMouseY) * this.camera.fov) / 600 +
+            this.mouseDownLat;
+    };
+    onPointerUp = () => {
+        this.mouseDownMouseX = null;
+    };
     animate = () => {
         requestAnimationFrame(this.animate);
+        this.lat = Math.max(-85, Math.min(85, this.lat));
+        this.phi = THREE.Math.degToRad(90 - this.lat);
+        this.theta = THREE.Math.degToRad(this.lon);
+        this.camera.target.x =
+            0.001 * Math.sin(this.phi) * Math.cos(this.theta);
+        this.camera.target.y = 0.001 * Math.cos(this.phi);
+        this.camera.target.z =
+            0.001 * Math.sin(this.phi) * Math.sin(this.theta);
+        this.camera.lookAt(this.camera.target);
         this.renderer.render(this.scene, this.camera);
     };
     render() {
